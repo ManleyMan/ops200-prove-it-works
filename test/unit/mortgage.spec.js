afd1eecc9bce4f5fae781704647f40c60704a042
ManleyMan <sweezymac@gmail.com>
2022-03-10 11:49:20 -0800

    wrote calculator unit tests

---
+++ test/unit/mortgage.spec.js
@@ -0,0 +1,14 @@
+const expect = require('chai').expect;
+const Mortgage = require('../../src/js/lib/Mortgage');
+
+describe('Mortgage Calculator', () => {
+    let mortgage = null;
+});
+
+beforeEach(() => {
+    monthlyPayment = new monthlyPayment();
+})
+
+it('should have monthlyPayment function', ()=> {
+    expect
+})
